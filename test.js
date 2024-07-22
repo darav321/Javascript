49d20421f37f511c195ea8b1b02692938b66d2fc
Varad <varadkale04@gmail.com>
2024-07-22 19:54:30 +0530

    variables understanding

--- test.js
+++ test.js
@@ -1,2 +1,22 @@
 console.log("Welcome to the world of Coding")
 console.log("Going for a second commit")
+
+
+// Understanding Variables
+const name = "Varad"
+let number = 12345
+var age = 20
+email = "varadkale04@gmail.com"
+let height
+// name = "pranav" will give error 
+
+/*
+never use var keyword 
+*/
+
+number = 88308
+age = 29
+email = "jfhuj@gmail.com"
+
+console.table([number, age, email, height])
+
